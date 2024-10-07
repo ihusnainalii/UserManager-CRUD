a88265727b1182784fb5cbb28709dac472ebb908
Husnain Ali <ali@ili.digital>
2024-10-07 23:41:31 +0500

    updated code
    updated code

--- crud-node-express/server.js
+++ crud-node-express/server.js
@@ -3,25 +3,28 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const UserRoute = require('./routes/User')
-const dbConfig = require('./config/database.config.js');
+const config = require('./config/database.config');
 const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use('/user',UserRoute)
+app.use('/user', UserRoute)
 
 app.get('/', (req, res) => {
     res.json({"message": "Hello Crud Node Express"});
 });
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(config.port, () => {
+    console.log(`Server is listening on port ${config.port}`);
 });
 
-mongoose.Promise = global.Promise;mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
+console.log(`Your app is ${config.app_name}`);
+console.log(`Your port is ${config.port}`);
+console.log(`Your db uri is ${config.db_uri}`);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.db_uri, {}).then(() => {
     console.log("Databse Connected Successfully!!");    
 }).catch(err => {
     console.log('Could not connect to the database', err);
